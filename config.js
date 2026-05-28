0e30e81f13e3642abb52ef713fc224cdb909e047
lavawulence <debouangabriceamour@gmail.com>
2026-05-28 13:03:14 +0100

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-export const BOT_PREFIX = '$';
-export const BOT_SECRET = 'LAVAWULE';
-export const OWNER_NUMBER = '242044106402';
-export const DAILY_LIMIT_PG = 100;
-export const PROOF_REWARD = 2;
